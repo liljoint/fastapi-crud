30cc9ac7a82af6357f018314c900422175a05235
Bryan Echegoyen <bryan.echegoyen@gmail.com>
2025-05-14 23:03:52 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# fastapi-crud
+
+Development by Bryan Echegoyen for Latam Airlines Full-stack job.
+
+# Highlights
+- Uvicorn for local implementations.
+- pytest for unit testing.
+- Docker for containers.
+- GCP Cloud Run implementations when commit on master.
--- app/main.py
+++ app/main.py
@@ -11,23 +11,23 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI()
 crud = CRUD()
 
-@app.get("/users")
+@app.get("/users", summary="Listar usuarios", description="Permite listar todos los usuarios registrados")
 async def listUsers(db: Session = Depends(get_db)):
     return await crud.listUsers(db)
 
-@app.get("/users/{id}")
+@app.get("/users/{id}", summary="Buscar usuarios", description="Permite buscar un usuario por ID")
 async def findUser(id: int,  db: Session = Depends(get_db)):
     return await crud.findUser(id, db)
     
-@app.post("/users")
+@app.post("/users", summary="Crear usuario", description="Permite crear un usuario mediante un objeto")
 async def addUser(user:UserCreateSchema, db: Session = Depends(get_db)):
     return await crud.addUser(user, db)
 
-@app.put("/users/{id}")
+@app.put("/users/{id}", summary="Actualizar usuario", description="Permite actualizar un usuario mediante su ID.")
 async def updateUser(id: int, user: UserUpdateSchema, db: Session = Depends(get_db)):
 
     return await crud.updateUser(id, user, db)
 
-@app.delete("/users/{id}")
+@app.delete("/users/{id}", summary="Borrar usuario", description="Permite borrar un usuario.")
 async def deleteUser(id: int, db: Session = Depends(get_db)):
     return await crud.deleteUser(id, db)
